b814f2dfe8d7cc52ae4578f7a6677416ac546a3a
lorenzosinisi <lasslo.net@gmail.com>
2019-04-19 12:45:36 +0200

    fix compiler warnings

--- lib/xjs.ex
+++ lib/xjs.ex
@@ -1,6 +1,6 @@
 defmodule XJS do
   def parse(str) do
-    with {:ok, tokens, _} <- :xjs_lexer.string(to_char_list(str)),
+    with {:ok, tokens, _} <- :xjs_lexer.string(to_charlist(str)),
          {:ok, result} <- :xjs_parser.parse(tokens) do
       result
     else
